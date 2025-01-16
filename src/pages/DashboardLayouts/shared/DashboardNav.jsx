9770fa8132ea5fa573d8a350542037dc59af278f
rasel mridha <raselmridha792@gmail.com>
2025-01-16 19:36:23 +0600

    set dashboard routes based on user role

--- src/pages/DashboardLayouts/shared/DashboardNav.jsx
+++ src/pages/DashboardLayouts/shared/DashboardNav.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
-import { FaAd, FaMoneyBill, FaUsers } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { FaAd, FaMoneyBill, FaUserAlt, FaUsers } from "react-icons/fa";
+import { IoIosNotifications } from "react-icons/io";
 import { IoMenu } from "react-icons/io5";
 import { LiaMoneyCheckAltSolid } from "react-icons/lia";
 import { MdOutlineHome } from "react-icons/md";
 import { TbCategoryFilled } from "react-icons/tb";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../../Context/AuthProvider";
+import { GiMedicines } from "react-icons/gi";
+import { FaMoneyBill1Wave } from "react-icons/fa6";
 
 const DashboardNav = () => {
   const [sidebar, setSidebar] = useState(false);
+  const { user } = useContext(AuthContext);
+  const userRole = "seller";
   return (
     <>
       <section className="bg-cyan-400 py-5 fixed w-full z-50">
@@ -21,9 +27,14 @@ const DashboardNav = () => {
             </button>
             <div className="flex justify-between items-center w-full">
               <h1 className="text-2xl uppercase font-bold">healShelf</h1>
-              <div className="avatar">
-                <div className="w-12 rounded-full">
-                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              <div className="flex items-center gap-5">
+                <button className="btn btn-ghost text-3xl">
+                  <IoIosNotifications />
+                </button>
+                <div className="avatar">
+                  <div className="w-10 rounded-full">
+                    <img src={user?.photoURL} />
+                  </div>
                 </div>
               </div>
             </div>
@@ -32,37 +43,102 @@ const DashboardNav = () => {
       </section>
       <section className="fixed lg:relative z-10">
         <div
-          className={`min-h-screen flex-col text-white pt-24 bg-gray-500 lg:flex w-72 ${
+          className={`min-h-screen flex-col text-white pt-24 bg-gray-500 lg:flex lg:w-96 ${
             sidebar ? "w-72" : "hidden"
           }`}
         >
           <h1 className="text-3xl uppercase text-center w-full">Dashboard</h1>
           <hr className="my-5" />
-          <div className="pl-5 text-lg capitalize">
+          <div className="text-lg capitalize">
             <nav>
-              <ul className="flex flex-col gap-3 hover:*:text-black">
-                <li className="flex items-center gap-1">
-                <MdOutlineHome className="text-2xl" /><NavLink to="/dashboard/admin-home">Admin Home</NavLink>
+              {userRole == "admin" && (
+                // admin routes
+                <ul className="flex menu items-start flex-col lg:text-xl">
+                  <li>
+                    <NavLink to="/dashboard/admin-home">
+                      <MdOutlineHome /> Admin Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="manage-user">
+                      <FaUsers /> manage users
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="manage-category">
+                      <TbCategoryFilled /> manage category
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="payment-management">
+                      <FaMoneyBill></FaMoneyBill> Payment Management
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="sales-report">
+                      <LiaMoneyCheckAltSolid /> sales report
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="banner-advertise">
+                      <FaAd></FaAd>
+                      Manage Banner Advertise
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+
+              {/* seller routes */}
+              {userRole == "seller" && (
+                <ul className="flex menu items-start flex-col lg:text-xl">
+                  <li>
+                    <NavLink to="/dashboard/seller-home">
+                      <MdOutlineHome /> Seller Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="manage-medicine">
+                      <GiMedicines />
+                      manage Medicine
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="payment-management">
+                      <FaMoneyBill1Wave /> Payment History
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="sales-report">
+                      <FaAd></FaAd> Ask for advertisement
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+
+              {/* seller routes  */}
+
+              {userRole == "customer" && (
+                <ul className="flex menu items-start flex-col lg:text-xl">
+                  <li>
+                    <NavLink to="payment-management">
+                      <FaMoneyBill1Wave /> Payment History
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </nav>
+            <hr className="my-5" />
+            <nav>
+              <ul className="menu flex flex-col text-xl">
+                <li>
+                  <NavLink to="/">
+                    <MdOutlineHome className="text-2xl" /> Home
+                  </NavLink>
                 </li>
-                <li className="flex items-center gap-1">
-                  <FaUsers></FaUsers>
-                  <NavLink to="manage-user">manage users</NavLink>
-                </li>
-                <li className="flex items-center gap-1">
-                <TbCategoryFilled />
-                  <NavLink to="manage-category">manage category</NavLink>
-                </li>
-                <li className="flex items-center gap-1">
-                  <FaMoneyBill></FaMoneyBill>
-                  <NavLink to="payment-management">Payment Management</NavLink>
-                </li>
-                <li className="flex items-center gap-1">
-                <LiaMoneyCheckAltSolid />
-                  <NavLink to="sales-report">sales report</NavLink>
-                </li>
-                <li className="flex items-center gap-1">
-                  <FaAd></FaAd>
-                  <NavLink to="banner-advertise">Manage Banner Advertise</NavLink>
+                <li>
+                  <NavLink to="/user-account">
+                    <FaUserAlt className="text-xl" /> Account
+                  </NavLink>
                 </li>
               </ul>
             </nav>
