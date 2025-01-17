c48f2c06716819fd7a53f224744561d1b9b7f30d
rasel mridha <raselmridha792@gmail.com>
2025-01-18 02:42:01 +0600

    create a slider using react swiperjs for discount products

--- src/pages/HomeLayouts/Discount.jsx
+++ src/pages/HomeLayouts/Discount.jsx
@@ -1,34 +1,89 @@
-import useProducts from "../DashboardLayouts/shared/loadDataHook/useProducts";
 import Loader from "../DashboardLayouts/shared/Loader";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import DiscountCard from "./DiscountCard";
+import { useEffect, useState } from "react";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const Discount = () => {
-    const [products, isLoading] = useProducts()
+  const axiosPublic = useAxiosPublic();
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      const res = await axiosPublic.get("/products");
+      const isDiscound = res.data.filter(
+        (product) => product.have_discount === true
+      );
+      setLoading(false);
+      setProducts(isDiscound);
+    };
+    loadData();
+  }, [axiosPublic]);
 
   return (
     <>
       <section className="my-20">
         <div className="w-full py-10 bg-cyan-400">
-          <div className="flex items-center justify-center">
+          <div className="flex items-center flex-col md:flex-row justify-center">
             <div>
-              <h1 className="text-4xl font-bold uppercase">
+              <h1 className="md:text-4xl text-2xl font-bold uppercase">
                 get upto 50% discount
               </h1>
               <p className="text-xl font-bold">for our OTC medicines</p>
             </div>
             <img
-              className="w-1/2 h-64 object-cover"
+              className="md:w-1/2 md:h-64 object-cover"
               src="https://i.ibb.co.com/1JGtSqK/pngegg-1.png"
               alt=""
             />
           </div>
         </div>
-        <div>
-          <h1 className="text-2xl py-5 capitalize">discount products</h1>
-          <div className="my-10">
-            {isLoading? <Loader></Loader> :  products.map(product => <div key={product._id}>
-                <h1>{product.name}</h1>
-            </div>)}
-        </div>          
+        <div className="my-10">
+          <h1 className="text-2xl font-bold pt-10 capitalize">discount products</h1>
+          <div className="my-5">
+            {loading ? (
+              <Loader></Loader>
+            ) : (
+              <>
+                <Swiper
+                  // install Swiper modules
+                  modules={[Navigation, Pagination, Scrollbar, A11y]}
+                  spaceBetween={50}
+                  //   slidesPerView={4}
+                  breakpoints={{
+                    320: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 2,
+                    },
+                    1024: {
+                      slidesPerView: 4,
+                    },
+                  }}
+                  navigation
+                //   pagination={{ clickable: true }}
+                //   scrollbar={{ draggable: true }}
+                >
+                  {products.map((product) => (
+                    <SwiperSlide key={product._id}>
+                      <DiscountCard product={product}></DiscountCard>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </>
+            )}
+          </div>
         </div>
       </section>
     </>
---
+++ src/pages/HomeLayouts/DiscountCard.jsx
@@ -0,0 +1,29 @@
+const DiscountCard = ({ product }) => {
+  const { name, image, _id } = product;
+
+  const handleViewDetails = (id) => {
+    console.log(id);
+  };
+  return (
+    <>
+      <div className="card card-compact bg-base-100 w-80 mx-5 border rounded-none">
+        <figure>
+          <img className="w-52 h-52 object-cover p-5" src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body flex flex-col min-h-[200px]">
+          <h2 className="card-title">{name}</h2>
+          <div className="card-actions mt-auto justify-end">
+            <button
+              onClick={() => handleViewDetails(_id)}
+              className="btn btn-outline btn-sm"
+            >
+              View More
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DiscountCard;
